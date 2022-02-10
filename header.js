152b2203e6c51cdd64d0bd135a4ace9b78535e21
Abhijeet016 <98399662+Abhijeet016@users.noreply.github.com>
2022-02-10 18:26:09 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,19 @@
+import { Button } from '@mui/material';
+import * as React from 'react';
+import { Toolbar } from '@mui/material';
+import { AppBar } from '@mui/material';
+import Typography from '@mui/material/Typography';
+export default function Header() {
+  return (
+    <>
+     <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            NineLeaps
+          </Typography>
+        </Toolbar>
+      </AppBar>
+     
+    </>
+  );
+}
